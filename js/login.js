c4ceb6167999b814f1d92685c3d80d397c192538
PlayerOne-bit <codexalpha24@gmail.com>
2025-10-21 03:12:13 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -17,10 +17,11 @@ function login(){
         && PASSWORD==="10212005")
     {
         sessionStorage.setItem("I'm Inside","true");
-        window.location.href='/index.html';
+        window.location.href='Bal-Website/index.html';
     }else{
         ERROR.innerText="Wrong username or password";
         inputs.forEach((e)=>{e.style.borderColor='red'});
     }
 
 }
+
